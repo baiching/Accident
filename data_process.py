e9795bcc6c21b3881dc118bc6e7f5066db151702
baiching <bachmarma@gmail.com>
2022-06-01 13:56:05 +0200

    Added GD and SGD

--- data_process.py
+++ data_process.py
@@ -6,11 +6,25 @@ Created on Wed May 25 15:34:32 2022
 """
 
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 
 data_X = pd.read_csv("./data/urbanGB.csv")
 data_Y = pd.read_csv("./data/urbanGB.labels.csv")
 
-# data_S = [data_X, data_Y]
+#data = pd.read_csv("./data/urbanGBmerged.csv")
+mx = MinMaxScaler()
+X_minmax = mx.fit_transform(data_X)
+Y_minmax = mx.fit_transform(data_Y)
 
-# nn = pd.concat(data_S)
-# print(nn)
+#print(X_train_minmax)
+def CrossValidation():
+    """
+    Returns (X_train, X_test, y_train, y_test)
+    -------
+    This functions calculates the cross validation for the dataset
+    Spliting data by 90/10 for training and test data 
+
+    """
+    return train_test_split(X_minmax, Y_minmax, test_size=0.1, random_state=0)
+
--- logistic_regression_demo.py
+++ logistic_regression_demo.py
@@ -5,17 +5,47 @@ Created on Wed May 25 16:05:17 2022
 @author: Uthowaipru Chowdhury
 """
 
-from data_process import data_X, data_Y
+#from data_process import CrossValidation
 import numpy as np
+from data_process import CrossValidation
 
 np.random.seed(0)
 W = np.random.randn(2)
 
 #print(len(data_X) - len(data_Y))
+X_train, X_test, y_train, y_test = CrossValidation()
+m = X_train.shape[0]
+print(X_train.shape)
 
-
-def Ein(W, X, y):
+def mean_square_error(W, X, y, m, lambd = 0.01):
     y_hat = np.dot(W.T, X.T)
-    return np.subtract(data_Y, y_hat.reshape(len(data_X), 1))
+    l2_reg = lambd * np.sum(W**2)
+    return np.sum(np.square(y - y_hat))/m + l2_reg, y_hat
 
-print(Ein(W, data_X, data_Y))
+def train(W, X_train, y_train, m, learning_rate=0.01):
+    for i in range(50):
+        error, y_hat = mean_absolute_error(W, X_train, y_train, m)
+        #dW = np.subtract(y_train, y_hat.reshape(len(y_hat), 1) * (- X_train)).mean()*2
+        dW = np.absolute(-X_train).mean()
+        W = W - learning_rate * dW
+        print("epoch "+ str(i)+ " " + str(error))
+        
+# print(train(W, X_train, y_train, m, 0.01))
+
+def SGD(W, X_train, y_train, m, batch_size, learning_rate=0.01, lambd = 0.01):
+    for epoch in range(30):
+        error_per_epoch = np.empty((m,2))
+        for i in range(m):
+            index = np.random.randint(0, m, batch_size)
+            X_i = np.take(X_train, indices=index)
+            y_i = np.take(y_train, indices=index)
+            
+            error, y_hat = mean_square_error(W, X_i, y_i, X_i.shape[0], lambd)
+            np.append(error_per_epoch, error)
+            
+            # update
+            W = W - learning_rate * (((-2/X_i.shape[0]) * (X_i.dot(y_i - y_hat))) + 2 * lambd * W)
+            
+        print("epoch " + str(i+1) + " :" + " loss: "+"accuracy :" + str(error_per_epoch.mean()))
+        
+SGD(W, X_train, y_train, m, 2, 0.01)
